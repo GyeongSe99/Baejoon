b3a6228ec300facfef6a81a922de2f59ac5d9096
jay45652 <sgcheon@naver.com>
2023-05-17 01:42:44 +0900

    2023-05-16

---
+++ Bronze1/_1157/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int[] getAlphabetCount(String str) {
+        str = str.toUpperCase();
+        int[] count = new int[26];
+        for (int i = 0; i < str.length(); i++) {
+            count[str.charAt(i) - 'A']++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int[] count = getAlphabetCount(str);
+
+        int maxCount = Integer.MIN_VALUE;
+        char maxAlphabet = '?';
+        for (int i = 0; i < count.length; i++) {
+            if (count[i] > maxCount) {
+                maxCount = count[i];
+                maxAlphabet = (char)('A' + i);
+            } else if (count[i] == maxCount) {
+                maxAlphabet = '?';
+            }
+        }
+        System.out.println(maxAlphabet);
+    }
+}
