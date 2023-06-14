238041af08bd0a6da75dc64620f9f35e68508c14
jay45652 <sgcheon@naver.com>
2023-06-15 00:46:46 +0900

    15651. N&M

---
+++ Silver3/_15651/src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] selected;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        selected = new int[M + 1];
+
+        rec_func(1);
+        System.out.print(sb.toString());
+    }
+
+    public static void rec_func(int k) {
+        if (k == M + 1) {
+            for (int i = 1; i <= M; i++) {
+                sb.append(selected[i]).append(' ');
+            }
+            sb.append('\n');
+        } else {
+            for (int i = 1; i <= N; i++) {
+                selected[k] = i;
+                rec_func(k + 1);
+                selected[k] = 0;
+            }
+        }
+    }
+}
